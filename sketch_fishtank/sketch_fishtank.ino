e0346caca32a1dc0a4c83cf6a33795154b706ceb
Bohdan Sushchak <bogdansushak@gmail.com>
2024-02-16 10:20:20 +0100

    Fetch settings from firebase, stream listen

--- sketch_fishtank/sketch_fishtank.ino
+++ sketch_fishtank/sketch_fishtank.ino
@@ -4,9 +4,7 @@
 #include "time.h"
 #include <Firebase_ESP_Client.h>
 
-// Provide the token generation process info.
 #include "addons/TokenHelper.h"
-// Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
 // Define Firebase objects
@@ -36,9 +34,20 @@ class Socket {
 
   bool stateOn;
 
+  bool isHourValid(int hour) {
+    return hour >= 0 && hour < 24;
+  }
+
+  bool isMinValid(int min) {
+    return min >= 0 && min < 60;
+  }
+
 public:
-  Socket(int pin) {
+  char *SOCKET_NAME;
+
+  Socket(int pin, char *name) {
     SOCKET_PIN = pin;
+    SOCKET_NAME = name;
     hourFrom = -1;
     minFrom = -1;
     hourTo = -1;
@@ -52,25 +61,34 @@ public:
   }
 
   void setTimePeriods(int hFrom, int mFrom, int hTo, int mTo) {
-    hourFrom = hFrom;
-    minFrom = mFrom;
-    hourTo = hTo;
-    minTo = mTo;
+    if (isHourValid(hFrom) && isMinValid(mFrom) && isHourValid(hTo) && isMinValid(mTo)) {
+      hourFrom = hFrom;
+      minFrom = mFrom;
+      hourTo = hTo;
+      minTo = mTo;
+      Serial.print("setTimePeriods ");
+      Serial.print(SOCKET_NAME);
+      Serial.println("success :)");
+    } else {
+      Serial.print("setTimePeriods");
+      Serial.print(SOCKET_NAME);
+      Serial.println("not valid");
+    }
   }
 
-  void updateState(struct tm *info) {
+  void updateState(struct tm *time) {
     if (hourFrom == -1 | minFrom == -1 | hourTo == -1 | minTo == -1) {
-      // Serial.print("Socket ");
-      // Serial.print(SOCKET_PIN);
-      // Serial.println(" time periods non set");
+      Serial.print("Socket ");
+      Serial.print(SOCKET_NAME);
+      Serial.println(" time periods non set");
       return;
     }
 
-    int currentHour = info->tm_hour;
-    int currenMin = info->tm_min;
-    int currentSec = info->tm_sec;
+    int currentHour = time->tm_hour;
+    int currenMin = time->tm_min;
+    int currSec = time->tm_sec;
 
-    long timeInSec = currentSec + (currenMin * 60) + currentHour * (3600);
+    long timeInSec = (currentHour * 3600) + (currenMin * 60) + currSec;
     long fromInSec = (hourFrom * 3600) + (minFrom * 60);
     long toInSec = (hourTo * 3600) + (minTo * 60);
 
@@ -81,30 +99,24 @@ public:
     }
   }
 } sockets[4] = {
-  { 1 },
-  { 2 },
-  { 3 },
-  { 4 },
+  { 12, "socket1" },
+  { 13, "socket2" },
+  { 14, "socket3" },
+  { 15, "socket4" },
 };
 
 void setup() {
   Serial.begin(115200);
-
   setupPins();
-
-  sockets[0].setTimePeriods(7, 0, 15, 0);
-  sockets[1].setTimePeriods(9, 0, 13, 0);
-  // sockets[2].setTimePeriods(10, 18, 10, 20);
-  // sockets[3].setTimePeriods(10, 19, 10, 22);
 
   setupWifi();
   setupCurrentTime();
-  // initFirebase();
+  initFirebase();
 }
 
 void loop() {
   timeLoop();
-  // firebaseLoop();
+  firebaseLoop();
 }
 
 //------ start WIFI setup
@@ -113,7 +125,7 @@ void setupWifi() {
   delay(1000);
 
   WiFi.onEvent(wifiStationConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);
-  // WiFi.onEvent(wifiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+  WiFi.onEvent(wifiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Wait for WiFi... ");
@@ -130,9 +142,11 @@ void wifiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info) {
 void wifiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
   Serial.println("Disconnected from WiFi access point");
   Serial.print("WiFi lost connection. Reason: ");
-  Serial.println(info.wifi_sta_disconnected.reason);
-  Serial.println("Trying to Reconnect");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  // Serial.println(info.wifi_sta_disconnected.reason);
+  // Serial.println("Trying to Reconnect");
+  // WiFi.disconnect();
+  // WiFi.reconnect();
+  // WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 //------ end WIFI setup
@@ -153,29 +167,49 @@ void streamCallback(FirebaseStream data) {
                 data.dataType().c_str(),
                 data.eventType().c_str());
   printResult(data);  // see addons/RTDBHelper.h
+  Serial.println();
+
+  FirebaseJson *json = data.to<FirebaseJson *>();
+
+  if (data.eventType().equals("/settings")) {
+    FirebaseJsonData settingsRes;
+    json->get(settingsRes, "settings");
+
+    if (settingsRes.success) {
+      FirebaseJson settingsJson;
+      settingsRes.getJSON(settingsJson);
+      updateSocketsSettings(settingsJson);
+    }
+  }
+
+  if (data.eventType().equals("/state")) {
+    FirebaseJsonData stateRes;
+    json->get(stateRes, "state");
+
+    if (stateRes.success) {
+      FirebaseJson stateJson;
+      stateRes.getJSON(stateJson);
+      updateSocketsState(stateJson);
+    }
+  }
 }
 
 void initFirebase() {
   config.api_key = FB_API_KEY;
   config.database_url = FB_DATABASE_URL;
+  config.token_status_callback = tokenStatusCallback;
+  config.timeout.serverResponse = 10 * 1000;
+  config.max_token_generation_retry = 5;
+
+  fbdo.setBSSLBufferSize(2048, 1024);
+  fbdo.setResponseSize(2048);
+  fbdo.keepAlive(5, 5, 1);
 
   auth.user.email = FB_USER_EMAIL;
   auth.user.password = FB_USER_PASSWORD;
 
   Firebase.reconnectWiFi(true);
-  fbdo.setBSSLBufferSize(4096, 4096);
-  fbdo.setResponseSize(4096);
-
-  // Assign the callback function for the long running token generation task
-  config.token_status_callback = tokenStatusCallback;
-  Firebase.setDoubleDigits(5);
-  config.timeout.serverResponse = 10 * 1000;
-  config.max_token_generation_retry = 5;
-
   Firebase.begin(&config, &auth);
-  // #if defined(ESP32)
-  //   stream.keepAlive(5, 5, 1);
-  // #endif
 
   // Getting the user UID might take a few seconds
   Serial.println("Getting User UID");
@@ -187,97 +221,75 @@ void initFirebase() {
   Serial.print("User UID: ");
   Serial.println(auth.token.uid.c_str());
 
-  // set stream callbacks
-  // if (!Firebase.RTDB.beginStream(&fbdo, smartFishTankPath)) {
-  //   Serial.printf("stream begin error, %s\n\n", fbdo.errorReason().c_str());
-  // } else {
-  //   Serial.println("stream begin");
-  // }
+  Firebase.RTDB.setStreamCallback(&fbdo, streamCallback, streamTimeoutCallback);
 
-  // Firebase.RTDB.setStreamCallback(&fbdo, streamCallback, streamTimeoutCallback);
+  // set stream callbacks
+  if (!Firebase.RTDB.beginStream(&fbdo, smartFishTankPath)) {
+    Serial.printf("stream begin error, %s\n\n", fbdo.errorReason().c_str());
+  } else {
+    Serial.println("stream begin");
+  }
 }
 
 void firebaseLoop() {
   if (!Firebase.ready()) {
-    Serial.println("Firebase not ready");
     return;
   }
 
-  fetchInitialSettings();
-
-  // if (!Firebase.RTDB.readStream(&fbdo))
-  //   Serial.printf("sream read error, %s\n\n", fbdo.errorReason().c_str());
-
-  // if (fbdo.streamTimeout()) {
-  //   Serial.println("stream timed out, resuming...\n");
-
-  //   if (!fbdo.httpConnected())
-  //     Serial.printf("error code: %d, reason: %s\n\n", fbdo.httpCode(), fbdo.errorReason().c_str());
-  // }
-
-  // if (fbdo.streamAvailable()) {
-  //   Serial.printf("sream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
-  //                 fbdo.streamPath().c_str(),
-  //                 fbdo.dataPath().c_str(),
-  //                 fbdo.dataType().c_str(),
-  //                 fbdo.eventType().c_str());
-  //   printResult(fbdo);  // see addons/RTDBHelper.h
-  //   Serial.println();
-
-  // This is the size of stream payload received (current and max value)
-  // Max payload size is the payload size under the stream path since the stream connected
-  // and read once and will not update until stream reconnection takes place.
-  // This max value will be zero as no payload received in case of ESP8266 which
-  // BearSSL reserved Rx buffer size is less than the actual stream payload.
-  // Serial.printf("Received stream payload size: %d (Max. %d)\n\n", fbdo.payloadLength(), fbdo.maxPayloadLength());
-  // }
-  // if (!Firebase.RTDB.readStream(&stream)) {
-  //   Serial.printf("sream read error, %s\n\n", stream.errorReason().c_str());
-  // }
-
-  // if (stream.streamTimeout()) {
-  //   Serial.println("stream timeout, resuming...\n");
-  // }
-
-  // if (stream.streamAvailable()) {
-  //   Serial.printf("sream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\nvalue, %d\n\n",
-  //                 stream.streamPath().c_str(),
-  //                 stream.dataPath().c_str(),
-  //                 stream.dataType().c_str(),
-  //                 stream.eventType().c_str(),
-  //                 stream.intData());
-
-  //   if (stream.dataTypeEnum() == firebase_rtdb_data_type_json) {
-  //     FirebaseJson *json = stream.to<FirebaseJson *>();
-  //     Serial.println(json->raw());
-  //   }
-  // }
+  chekcAndFetchInitialSettings();
 }
 
-void fetchInitialSettings() {
+void chekcAndFetchInitialSettings() {
   if (initSettinsHasFetched) {
     return;
   }
   unsigned long currentMillis = millis();
-  if (initSettinsPrevMilis == 0 || currentMillis - initSettinsPrevMilis >= 10000) {
+  if (initSettinsPrevMilis == 0 || (currentMillis - initSettinsPrevMilis) >= 10000) {
     initSettinsPrevMilis = currentMillis;
 
     FirebaseJson settingsJson;
     Firebase.RTDB.getJSON(&fbdo, smartFishTankPath + String("/settings"), &settingsJson);
     Serial.println("Fetch initial settings");
-    Serial.println(settingsJson.raw());
     updateSocketsSettings(settingsJson);
+    initSettinsHasFetched = true;
   }
 }
 
 void updateSocketsSettings(FirebaseJson settingsJson) {
-  char *socketsNames[4] = { "socket1", "socket2", "socket3", "socket4" };
-  FirebaseJsonData socketRes;
-  for (int i = 0; i < 4; i++) {
-    settingsJson.get(socketRes, socketsNames[i]);
+  Serial.println("updateSocketsSettings called");
+
+  for (Socket socket : sockets) {
+    FirebaseJsonData socketRes;
+    settingsJson.get(socketRes, socket.SOCKET_NAME);
+    if (socketRes.type != "object") {
+      continue;
+    }
+
     if (socketRes.success) {
+      FirebaseJson socketJson;
+      socketRes.getJSON(socketJson);
+
+      FirebaseJsonData fromHourUtcRes, fromMinUtcRes, toHourUtcRes, toMinUtcRes;
+      int fromHourUtc, fromMinUtc, toHourUtc, toMinUtc;
+      socketJson.get(fromHourUtcRes, "fromHourUtc");
+      socketJson.get(fromMinUtcRes, "fromMinUtc");
+      socketJson.get(toHourUtcRes, "toHourUtc");
+      socketJson.get(toMinUtcRes, "toMinUtc");
+
+      if (fromHourUtcRes.success && fromMinUtcRes.success && toHourUtcRes.success && toMinUtcRes.success) {
+        fromHourUtc = fromHourUtcRes.to<int>();
+        fromMinUtc = fromMinUtcRes.to<int>();
+        toHourUtc = toHourUtcRes.to<int>();
+        toMinUtc = toMinUtcRes.to<int>();
+
+        socket.setTimePeriods(fromHourUtc, fromMinUtc, toHourUtc, toMinUtc);
+      }
     }
   }
+}
+
+void updateSocketsState(FirebaseJson stateJson) {
+  Serial.println("updateSocketsState called");
 }
 //------ end firebase
 
@@ -290,7 +302,7 @@ void setupCurrentTime() {
 
 void printCurrentTime() {
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo, 1000)) {
+  if (!getLocalTime(&timeinfo, 500)) {
     Serial.println("Failed to obtain time");
     return;
   }
@@ -306,10 +318,10 @@ void timeLoop() {
     previousTimeMillis = currentMillis;
     struct tm timeinfo;
     Serial.println("timeLoop");
-    if (!getLocalTime(&timeinfo)) {
+    if (!getLocalTime(&timeinfo, 500)) {
       return;
     }
-    // printCurrentTime();
+    printCurrentTime();
 
     for (Socket socket : sockets) {
       socket.updateState(&timeinfo);
